414291c3f06eecb430f4006b3ab5eba2f3c5696d
Daniel Barros <barros.daniel@outlook.com>
2019-04-05 08:40:07 +0800

    feature: Add Loader image functionality

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 // Listen for calculate
-document
-  .getElementById("loan-form")
-  .addEventListener("submit", calculateResults);
+document.getElementById("loan-form").addEventListener("submit", function(e) {
+  // Hide results
+  document.getElementById("results").style.display = "none";
+
+  // Show loader
+  document.getElementById("loading").style.display = "block";
+
+  // Calculate results
+  setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
 
 // Calculate results
 function calculateResults(e) {
-  e.preventDefault();
   console.log("Calculating");
   const amount = document.getElementById("amount");
   const interest = document.getElementById("interest");
@@ -26,12 +34,20 @@ function calculateResults(e) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+
+    // Show results and hide loader
+    document.getElementById("results").style.display = "block";
+    document.getElementById("loading").style.display = "none";
   } else {
     showError("Please check your numbers.");
   }
 }
 
 function showError(error) {
+  // Hide results and loader
+  document.getElementById("results").style.display = "none";
+  document.getElementById("loading").style.display = "none";
+
   // Create a div
   const errorDiv = document.createElement("div");
 
